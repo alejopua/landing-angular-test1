68804221391b798b8beb573f0730c35f02e65096
alejopua <alejopua@gmail.com>
2024-04-12 12:56:46 -0500

    Construction the  contact component to use template-driven form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+// TEMPLETE DRIVEN FORM
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
+  public user: any = {
+    name: '',
+    email: '',
+  };
 
+  submitForm() {
+    console.log(this.user);
+  }
 }
