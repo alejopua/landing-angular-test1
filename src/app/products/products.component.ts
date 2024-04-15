904d605f7542f4bd3b03e785b08adf93cc9501f9
alejopua <alejopua@gmail.com>
2024-04-15 16:05:02 -0500

    Update ProductsComponent to use ApiProductsService for data retrieval

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-import { productsList } from './products.mock';
+import { Component, OnInit } from '@angular/core';
+import { IProducts } from '../models/product.model';
+import { ApiProductsService } from '../services/api-products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {
-  products = productsList;
+export class ProductsComponent implements OnInit {
+  products: IProducts[] = [];
+
+  constructor(private _apiService: ApiProductsService) {}
+
+  ngOnInit(): void {}
 }
